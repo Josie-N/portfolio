0fd947ac5b5bf8b8244fe5e5a114fdb5b023c945
Josie <nagy.iozefina@yahoo.com>
2018-07-23 21:45:49 +0300

    Initial Commit

---
+++ jquery/lazyload.js
@@ -0,0 +1,11 @@
+$("img.lazy").lazyload({
+    effect : "fadeIn"
+});
+
+/*
+jQuery(document).ready(function ($) {
+    $("img.lazy").lazyload(
+        { data_attribute: "orig"
+        });
+});
+*/
